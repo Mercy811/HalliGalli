45b02c58389501ff68e7bade1645a2cb10fa33d0
gracwang <734264057>
2019-06-17 10:11:47 +0800

    选择头像页面

--- assets/scripts/Card.js
+++ assets/scripts/Card.js
@@ -1,26 +1,24 @@
-// 种类：
-// Straberry, Banana, Plum, Lemon
-// 点数：
-// 1, 2, 3, 4, 5
-// 对应点数的牌的张数
-// 3, 3, 3, 3, 2
+// type:
+// fruit, monkey, pig, elephant
+// s, b, p, l：
+// straberry, banana, plum, lemon
 
 
 // String type
-// Int point 
-function Card(type, point){
-    this.type = type;   //种类
-    this.point = point; //水果个数(点数)
-    this.loadName = type + point.toString();
+// Int s,b,p,l 
+function Card(type, s, b, p, l){
+    this.type = type;
+    if(type === "fruit"){
+        this.straberry = s;
+        this.banana = b;
+        this.plum = p;
+        this.lemon = l;
+        this.loadName = s.toString()+ b.toString()+ p.toString() + l.toString();
+    }else{
+        this.loadName = type;
+    }
 }
 
-Card.prototype.add = function (card2){
-    if(this.type === card2.type){
-        return this.point + card2.point;
-    }else{
-        return -1;
-    }
-};
 
 // 把函数 Card 作为模块的输出暴露出去，这样其他模块就可以使用 Card 函数了
 module.exports = Card;
--- assets/scripts/Cards.js
+++ assets/scripts/Cards.js
@@ -1,38 +1,64 @@
 var Card = require("./Card");
 
-const TYPE = ["", "Straberry", "Banana", "Plum", "Lemon"];
+//const TYPE = ["", "Straberry", "Banana", "Plum", "Lemon"];
 
 // 一副牌
-function Cards(){
-    this.cardNum = 56;
+function Cards() {
+    this.cardNum = 98;
     this.content = [];
-    for(var i=1;i<=4;i++){
-        for(var m=0;m<3;m++){
-            this.content.push(new Card(TYPE[i],1));
-        }
-        for(var m=0;m<3;m++){
-            this.content.push(new Card(TYPE[i],2));
-        }
-        for(var m=0;m<3;m++){
-            this.content.push(new Card(TYPE[i],3));
-        }
-        for(var m=0;m<3;m++){
-            this.content.push(new Card(TYPE[i],4));
-        }
-        for(var m=0;m<2;m++){
-            this.content.push(new Card(TYPE[i],5));
+
+    //基础牌 56
+    for (var n = 1; n <= 4; n++) {
+        for (var i = 1; i <= 3; i++) {
+            this.content.push(new Card("fruit", n, 0, 0, 0));
+            this.content.push(new Card("fruit", 0, n, 0, 0));
+            this.content.push(new Card("fruit", 0, 0, n, 0));
+            this.content.push(new Card("fruit", 0, 0, 0, n));
         }
     }
-    //console.log(this.content);
+    for (var i = 1; i <= 2; i++) {
+        this.content.push(new Card("fruit", 5, 0, 0, 0));
+        this.content.push(new Card("fruit", 0, 5, 0, 0));
+        this.content.push(new Card("fruit", 0, 0, 5, 0));
+        this.content.push(new Card("fruit", 0, 0, 0, 5));
+    }
+
+    //扩展牌 42
+    for (var i = 1; i <= 3; i++) { //30
+        for (var j = 1; j <= 3; j++) {
+            if(i!==1&&j!==1){
+                continue;
+            }
+            this.content.push(new Card("fruit", i, j, 0, 0));
+            this.content.push(new Card("fruit", i, 0, j, 0));
+            this.content.push(new Card("fruit", i, 0, 0, j));
+            this.content.push(new Card("fruit", 0, i, j, 0));
+            this.content.push(new Card("fruit", 0, i, 0, j));
+            this.content.push(new Card("fruit", 0, 0, i, j));
+        }
+    }
+    for(var i = 1; i <=3; i++){ //6
+        this.content.push(new Card("elephant",0,0,0,0));
+        this.content.push(new Card("monkey",0,0,0,0));
+    }
+    for(var i = 1; i <=2; i++){ //2
+        this.content.push(new Card("pig",0,0,0,0));
+    }
+
+    // 4
+    this.content.push(new Card("fruit", 1, 4, 0, 0));
+    this.content.push(new Card("fruit", 1, 1, 1, 1));
+    this.content.push(new Card("fruit", 1, 1, 1, 0));
+    this.content.push(new Card("fruit", 1, 1, 0, 1));
 }
 
-Cards.prototype.shuffle = function(){
+Cards.prototype.shuffle = function () {
 
     var input = this.content;
 
-    for (var i = input.length-1; i >=0; i--) {
+    for (var i = input.length - 1; i >= 0; i--) {
 
-        var randomIndex = Math.floor(Math.random()*(i+1));
+        var randomIndex = Math.floor(Math.random() * (i + 1));
         var itemAtIndex = input[randomIndex];
 
         input[randomIndex] = input[i];
